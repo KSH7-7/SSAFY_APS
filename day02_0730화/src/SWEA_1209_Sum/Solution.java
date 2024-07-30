65471c484dd42bd6333d1aca079463ac7980f431
KSH7-7 <ssafy12ksh@gmail.com>
2024-07-30 17:48:12 +0900

    240730 17:48

--- day02_0730화/src/SWEA_1209_Sum/Solution.java
+++ day02_0730화/src/SWEA_1209_Sum/Solution.java
@@ -1,5 +1,51 @@
 package SWEA_1209_Sum;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = 100;
+		int[][] arr = new int[N][N];
+
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		int sumex[] = new int[N];
+		int sumwhy[] = new int[N];
+		int sumN = 0;
+		int sumM = 0;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				sumex[i] += arr[i][j];
+				sumwhy[j] += arr[i][j];
+			}
+		}
+		int maxra = 0;
+		int maxnm = 0;
+		for(int i = 0; i < N; i++) {
+				sumN += arr[i][i];
+				sumM += arr[i][N-1-i];
+		}
+		
+		for(int i=1; i<N; i++) {
+			maxra = Math.max(maxra, sumex[i]);
+            maxra = Math.max(maxra, sumwhy[i]);
+		
+		maxnm = Math.max(sumN, sumM);
+		}
+		
+		int totmax = Math.max(maxnm, maxra);
+            System.out.print("#" + test_case + " ");
+            System.out.println(totmax);
+		}
+	}
 }
