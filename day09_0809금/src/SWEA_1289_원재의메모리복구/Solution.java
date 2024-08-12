eb8c5345c22c1d38136c4db9064bc447d15a0da0
KSH7-7 <ssafy12ksh@gmail.com>
2024-08-12 17:59:07 +0900

    24.08.12. 17:59

--- day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
+++ day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
@@ -1,5 +1,40 @@
 package SWEA_1289_원재의메모리복구;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileInputStream;
 
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+        int i;
+        ArrayList<Integer> AL = new ArrayList<>();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int length = 0;
+			int cnt = 0;
+             for (i = 0; i < length; i++) {
+            	 int value = sc.nextInt();
+            	 AL.add(value);
+             }
+             for (i = 0; i < length; i++) {
+            if ( AL.get(i) == 0 && AL.get(i+1) == 1) {
+                    cnt++;
+                }
+                if (AL.get(i) == 1 && AL.get(i+1) == 0) {
+                    cnt++;
+                    if (AL.get(i) == 1) {
+                        cnt++;
+                    }
+                }
+             }
+            System.out.println("#" + test_case + " " + cnt);
+	}
+		sc.close();
+}
 }
